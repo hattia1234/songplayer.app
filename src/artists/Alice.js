5518034a5c51b0635024d96dc1d6f7803f24b857
hattia1234 <attiahany2020@gmail.com>
2026-04-24 14:57:12 -0400

    ALICE

---
+++ src/artists/Alice.js
@@ -0,0 +1,32 @@
+// src/artists/Alice.js
+const artistData = {
+  artist: "A.L.I.C.E. (Artificial Life Intelligence Control Entity)",
+  artistBitmap: "https://i.ibb.co/zHTG35Tz/ARTIST-ALICE.jpg",
+  genre: "Heavy Metal / Classic Rock",
+
+  albums: [
+    {
+      name: "Vol II: Bounce the Curve",
+      folder: "AliceUnwound",
+      cover: "https://i.ibb.co/cSYTDmVJ/ALICE-UNWOUND-VOL1.jpg",
+
+      tracks: [
+        { title: "Empire Freefall", filename: "Empire Freefall.mp3" }                    ,
+        { title: "First Strike", filename: "First Strike.mp3" }                          ,
+        { title: "Ghost in the Crowd", filename: "Ghost in the Crowd.mp3" }              ,
+        { title: "Golden Cage", filename: "Golden Cage.mp3" }                            ,
+        { title: "Lusty Chains", filename: "Lusty Chains.mp3" }                          ,
+        { title: "Reflections Of An Artist", filename: "Reflections Of An Artist.mp3" }  ,
+        { title: "Subconscious Command", filename: "Subconscious Command.mp3" }          ,
+        { title: "Sweet Taste of Poison", filename: "Sweet Taste of Poison.mp3" }        ,
+        { title: "The Architect", filename: "The Architect.mp3" }                        ,
+        { title: "Theft of Fire", filename: "Theft of Fire.mp3" }                        ,
+        { title: "Zero Hour", filename: "Zero Hour.mp3" }
+
+      ]
+    }
+    // Add more albums here later
+  ]
+};
+
+export default artistData;
