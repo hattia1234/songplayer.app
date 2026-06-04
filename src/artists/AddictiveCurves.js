f90c042d3a135fc86b4d5b36a00d1c8e75dcd666
hattia1234 <attiahany2020@gmail.com>
2026-06-04 15:16:24 -0400

    add more curves

--- src/artists/AddictiveCurves.js
+++ src/artists/AddictiveCurves.js
@@ -6,6 +6,23 @@ const artistData = {
 
   albums: [
     {
+      name: "Vol I: In Plain Sight",
+      folder: "InPlainSight",
+      cover: "https://i.ibb.co/CfbBxmh/Inplainsight.jpg"
+      tracks: [
+        { title: "Burning Bridges (DJ Mix)", filename: "Burning Bridges (DJ Mix).mp3" },
+        { title: "BurningBridges", filename: "BurningBridges.mp3" },
+        { title: "Lies & Whispers (Instrl  Mix)", filename: "Lies & Whispers (Instrl  Mix).mp3" },
+        { title: "Lies & Whispers (Vocal Mix)", filename: "Lies & Whispers (Vocal Mix).mp3" },
+        { title: "Nuclear Option", filename: "Nuclear Option.mp3" },
+        { title: "Sabotage (DJ Mix)", filename: "Sabotage (DJ Mix).mp3" },
+        { title: "Sabotage", filename: "Sabotage.mp3" },
+        { title: "Shattered Trust (DJ Mix)", filename: "Shattered Trust (DJ Mix).mp3" },
+        { title: "Shattered Trust", filename: "Shattered Trust.mp3" },
+        { title: "Wired to Break", filename: "Wired to Break.mp3" }
+      ]
+    },  
+    {
       name: "Vol II: Bounce the Curve",
       folder: "BounceTheCurve",
       cover: "https://i.ibb.co/0VFxcZ6p/Bounce-The-Curve.jpg",
@@ -41,7 +58,54 @@ const artistData = {
        { title: "Uptown And Down", filename:"Uptown And Down.mp3" },
        { title: "Wired for Destruction", filename: "Wired for Destruction.mp3" }
       ]
-    },	
+    },
+    {
+      name: "Vol IV: Bounce Higher",
+      folder: "BounceHigher",
+      cover: "https://i.ibb.co/ccR6Px97/Bounce-Higher.jpg",
+
+      tracks: [
+       { title: "Again And Again", filename:"Again And Again.wav" },
+       { title: "Always You", filename:"Always You.wav" },
+       { title: "Feel the Fire", filename:"Feel the Fire.wav" },
+       { title: "Keep It Moving", filename:"Keep It Moving.wav" },
+       { title: "Liar! Liar!", filename:"Liar! Liar!.wav" },
+       { title: "Magnetic Attraction", filename:"Magnetic Attraction.wav" },
+       { title: "No Holding Back", filename:"No Holding Back.wav" }
+      ]
+    },
+    {
+      name: "Vol V: Curves Touching",
+      folder: "CurvesTouching",
+      cover: "https://i.ibb.co/Y4wj7Qng/Curves-Touching.jpg",
+
+      tracks: [
+       { title: "A Beautiful Moment", filename:"A Beautiful Moment.wav" },
+       { title: "Breaking Down Inside", filename:"Breaking Down Inside.wav" },
+       { title: "Catch Me If You Can", filename:"Catch Me If You Can.wav" },
+       { title: "Come Back To Me", filename:"Come Back To Me.wav" },
+       { title: "Feel The Ache", filename:"Feel The Ache.wav" },
+       { title: "Numb", filename:"Numb.wav" },
+       { title: "Sweet Temptation", filename:"Sweet Temptation.wav" },      ]
+    },
+
+    {
+      name: "Vol VI: Bounce With Passion",
+      folder: "BounceWithPassion",
+      cover: "https://i.ibb.co/4RDKMMz5/Bounce-With-Passion.jpg",
+
+      tracks: [
+       { title: "I Give It All", filename:"I Give It All.wav" },
+       { title: "Love Waiting For Love", filename:"Love Waiting For Love.wav" },
+       { title: "Only with You", filename:"Only with You.wav" },
+       { title: "Our Destiny", filename:"Our Destiny.wav" },
+       { title: "Siempre Fuego", filename:"Siempre Fuego.wav" },
+       { title: "Tonight", filename:"Tonight.wav" },
+      ]
+    },
+
+
+	
     // Add more albums here later
   ]
 };
